14aedb49c694a06cdc0bbab704406f70fe44ee80
AgustinMoralejo <amoralejo@fi.uba.ar>
2022-10-01 11:09:31 -0300

    feat host config file

--- src/start-server.py
+++ src/start-server.py
@@ -13,20 +13,31 @@ def read_port():
     for line in lines:
         port_line = line.split('=')
         if port_line[0] == 'port':
-            port = port_line[1]
-    config_file.close() 
+            port = port_line[1].rstrip()
+    config_file.close()
     return port
+
+def read_host():
+    """Read the config file and get the host"""
+    config_file = open('config.txt','r')
+    lines = config_file.readlines()
+    for line in lines:
+        host_line = line.split('=')
+        if host_line[0] == 'host':
+            host = host_line[1]
+    config_file.close() 
+    return host 
 
 @click.command()
 @click.option('-v', '--verbose', default=1, help='increase output verbosity')
 @click.option('-q', '--quiet', default=1, help='decrease output verbosity')
-@click.option('-H', '--host', default=1, help='service IP address')
+@click.option('-H', '--host', default=read_host, help='service IP address')
 @click.option('-p', '--port', default=read_port, help='service port')
 @click.option('-s', '--storage', default='files', help='storage dir path')
 
 def main(verbose, quiet, host, port, storage):
     """Comando para comenzar el servidor del custom-ftp"""
-    server = TcpLiteServer((port, 10563))
+    server = TcpLiteServer((port, int(host)))
     for sock in server.listen():
         msg = None
         msg = sock.receive().decode('ASCII')
